b385c709f26823622f564d8a088bcda5e8a9e7bc
Danendz <danilapayne@gmail.com>
2022-07-10 15:57:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Stage, PerspectiveCamera } from "@react-three/drei";
+import Model from "./Model";
 
-function App() {
+export default function Viewer() {
+  const ref = useRef();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{position: 'absolute', width: '100%', textAlign: 'center', zIndex: '101'}}>Hello</div>
+      <Canvas shadows dpr={[1, 2]}>
+        <PerspectiveCamera makeDefault fov={50} position={[5, 0.5, 10]} />
+        <Suspense fallback={null}>
+          <Stage controls={ref} intensity={1}>
+            <Model />
+          </Stage>
+        </Suspense>
+        <OrbitControls
+          makeDefault
+          maxDistance={28}
+          minDistance={10}
+          minPolarAngle={1.35}
+          maxPolarAngle={2}
+          enablePan={true}
+          ref={ref}
+        />
+      </Canvas>
+    </>
   );
 }
-
-export default App;
